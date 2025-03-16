845737e6b3c2d73d277eb41d0eb04b25aa420f4d
Chun-Feng Liao <cfliao@nccu.edu.tw>
2025-03-16 13:21:51 +0800

    modified:   http/SimpleHttpClient.js 	new file:   http/howToShowSimpleBrowser.txt

--- http/SimpleHttpClient.js
+++ http/SimpleHttpClient.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const https = require('https');
 
 const options = {
-    host: 'cfliao.net',
-    port: 80,
-    path: '/cht/doku.php',
+    host: 'cfliao.nccu.edu.tw',
+    port: 443,
+    path: '/',
     method: 'GET'
 };
 
 
-http.get(options, (resp)=>{
+https.get(options, (resp)=>{
     resp.setEncoding('utf-8');
     console.log(resp.headers);
     resp.on('data', function (data) {
